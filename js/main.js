72b68d12e0e04c07e539c948b5ebd731801aa009
aozog <artur.ozog@gmail.com>
2020-08-26 15:29:18 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function kobe(n, x) {
+    if (n===20) {
+        return "true";
+    } else if ((n + x)<=20) {
+        return "true";
+    } else {
+        return "false";
+    };
+};
+
+
+
+console.log(kobe(n, x));
+
